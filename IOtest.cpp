e30b66a1e263bdb1b183b58b27015eccfba61812
박성빈 <80455552+Seongbin2@users.noreply.github.com>
2021-06-29 22:38:43 +0900

    Create IOtest.cpp

---
+++ IOtest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+#include <cstdio>
+
+int main() {
+	int A, B;
+	std::cout << "A를 입력하세요" << std::endl;
+	std::cin >> A;
+	std::cout << "B를 입력하세요" << std::endl;
+	std::cin >> B;
+
+	std::cout << "A는 " << A << "이고, B는 " << B << "이다." << std::endl;
+
+	return 0;
+}
